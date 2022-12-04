4d21b7fcfa426e9c8775504d1f8bbc92b31fc58b
artiomgazha <118769635+artiomgazha@users.noreply.github.com>
2022-12-04 23:58:19 +0300

    Update task3.py

--- Лабораторная работа 4/task3.py
+++ Лабораторная работа 4/task3.py
@@ -10,9 +10,6 @@ def delete(list_, index=-1):
     else:
         return left_list + right_list # сложение двух частей при отсальных случаях
 
-
-
-
 print(delete([0, 1, 2], index=0))  # [1, 2]
 print(delete([0, 1, 2], index=1))  # [0, 2]
 print(delete([0, 1, 2]))  # [0, 1]
